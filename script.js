878d702e7a13c086e81c62f44c26798285859128
Samuel Santinelli <samuelsantinelli@gmail.com>
2021-11-08 16:57:31 -0300

    aa

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
-"use scrict";
+'use strict';
 
-function pesquisarImagens(){
-    var imagens=document.getElementById('imagens').value;
-
-         fetch(`https://pixabay.com/api/?key=24137333-2a41ce31f4918e225e2b129a2&q=${imagens}&image_type=photo`).then(function(response){
-                        response.json()
-
-                        .then((data) => {
-                            console.log(data);
-                            var  largeImageURL= data.hits[0].largeImageURL;
-                            var img="<img src='"+largeImageURL+"' height=300 width=400/>"
-
-                            document.getElementById('result').innerHTML=img;
-
-                        });
-                
-                    });
-
-                    const createItem = (largeImageURL) => {
-                        const container = document.querySelector("#result")
-                        const newLink = document.createElement("a")
-                        newLink.href = `#${getId(largeImageURL)}`
-                        newLink.classList.add("galery-items")
-                        newLink.innerHTML = `<img src="${largeImageURL}" alt=""/>`
-                        container.appendChild(newLink)
-                    
-                    }
-            
+    const limparElementos = (elemento) => {
+        while(elemento.firstChild){
+            elemento.removeChild(elemento.lastChild);
+        }
+    }
 
 
+const insertNamePicture  = async (evento) =>{
+    if(evento.key == "Enter" || evento.type == "click"){ 
+    const imagems = document.getElementById('pesquisaImagens').value;
+    const url = `https://pixabay.com/api/?key=24136613-919a2d4521b496eb21144bbaa&q=${imagems}&image_type=photo`;
+    const dados = await fetch(url);
+    const fotos = await dados.json(); 
+
+    limparElementos(document.querySelector('.galery-container'))
+   
+   carregarImagens(fotos.hits);
+    
+ }
+   
+   
 }
+
+
+const uploadPictures = (item) => {
+    const img = document.querySelector('.galery-container');
+    const div = document.createElement('div')
+    div.classList.add('picture')
+    div.innerHTML = `
+    <div class="imgPicture"> 
+    <img id="Picture" src="${item.previewURL}">
+    <div class="imgPicture">
+    <div class="typeDesc">${item.tags}</div>
+    <div class="typeDesc">${item.likes} likes <img  class="icon-right" src="imagens/like.png"></div>
+    <div class="typeDesc">${item.comments} comments <img  class="icon-right" src="imagens/comment.png"></div>
+</div>
+ `
+
+    
+img.appendChild(div);
+}
+
+
+const carregarImagens = (imagem) => imagem.forEach(uploadPictures) 
+
+
+
+
+document.getElementById('pesquisaImagens').addEventListener('keypress' ,insertNamePicture);
+
+document.querySelector('.material-icons').onclick = insertNamePicture
+
+
